76024bef0f9b51c9176dbcb7554c845cebfbc25d
Daniil Rainchik <danikrainchik2007@gmail.com>
2025-10-07 14:30:09 +0300

    Added structure of custom list and its test

---
+++ src/main/java/com/customlinkedlist/RainchikLinkedList.java
@@ -0,0 +1,45 @@
+package com.customlinkedlist;
+
+public class RainchikLinkedList {
+
+    public int size(){
+        return 0;
+    }
+
+    public void addFirst(Object el){
+
+    }
+
+    public void addLast(Object el){
+
+    }
+
+    public void add(int index, Object el){
+
+    }
+
+    public Object getFirst(){
+        return null;
+    }
+
+    public Object getLast(){
+        return null;
+    }
+
+    public void get(int index){
+
+    }
+
+    public void removeFirst(){
+
+    }
+
+    public void removeLast(){
+
+    }
+
+    public void remove(int index){
+
+    }
+
+}
---
+++ src/test/java/RainchikLinkedListTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+public class RainchikLinkedListTest {
+
+    @Test
+    public void size(){
+
+    }
+
+    @Test
+    public void addFirst(){
+
+    }
+
+    @Test
+    public void addLast(){
+
+    }
+
+    @Test
+    public void add(){
+
+    }
+
+    @Test
+    public void getFirst(){
+
+    }
+
+    @Test
+    public void getLast(){
+
+    }
+
+    @Test
+    public void get(){
+
+    }
+
+    @Test
+    public void removeFirst(){
+
+    }
+
+    @Test
+    public void removeLast(){
+
+    }
+
+    @Test
+    public void remove(){
+
+    }
+}
